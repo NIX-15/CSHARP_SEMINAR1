705106f371aaa8218a6e438148a2537f7f22198f
Nikita Sechenov <nikitasechenov@MacBook-Pro-Nikita.local>
2023-05-30 19:57:47 +0300

    task1

---
+++ seminar1/task1/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Input The Number: ");
+string input = Console.ReadLine();
+int i = Convert.ToInt32(input);
+int q = i*i;
+Console.Write("Squad of the number is: ");
+Console.WriteLine(q);
