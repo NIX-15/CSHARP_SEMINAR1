85966d0bc41b0bf56012ec74cf3e137a424e17d8
Nikita Sechenov <nikitasechenov@MacBook-Pro-Nikita.local>
2023-05-30 20:40:52 +0300

    task3

---
+++ seminar1/task2/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Input Two Numbers To Compare: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int squad = Convert.ToInt32(Console.ReadLine());
+if ( squad == number*number||number == squad*squad )
+{
+   Console.WriteLine("Correct "); 
+   Console.Write() 
+}
+else
+{
+Console.WriteLine("Error. Try again!"); 
+}
