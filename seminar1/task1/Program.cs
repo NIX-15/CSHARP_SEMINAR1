1e9dd3ad14c5de71c0317fe8a0b6bdca691dcde9
Nikita Sechenov <nikitasechenov@MacBook-Pro-Nikita.local>
2023-05-30 20:00:14 +0300

    task1.1

--- seminar1/task1/Program.cs
+++ seminar1/task1/Program.cs
@@ -1,6 +1,3 @@
 ﻿Console.Write("Input The Number: ");
-string input = Console.ReadLine();
-int i = Convert.ToInt32(input);
-int q = i*i;
-Console.Write("Squad of the number is: ");
-Console.WriteLine(q);
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(number*number);
