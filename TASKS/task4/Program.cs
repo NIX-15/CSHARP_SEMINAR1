c5570c83265fb01bf1951c8a50183744c16b26ee
Nikita <nix15@bk.ru>
2023-06-01 20:40:10 +0300

    task4

---
+++ TASKS/task4/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Input Your Number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int i = -num;
+while (i >= -num && i <= num)
+{
+    Console.Write(i);
+    Console.Write(" ");
+    i++;
+}
