44fdaad7bcdf6360e0d9b48365418a489af9b144
Nikita <nix15@bk.ru>
2023-06-01 21:18:10 +0300

    task5_1/2

---
+++ TASKS/task5/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Input Your Number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+while (num < -999 || (num > -100 && num < 100) || num > 999)
+{
+    Console.Write("Input Your Number Again: ");
+    num = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine(num);
