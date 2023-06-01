2785eddc81fc17231702073ffc96dbf973da8b8d
Nikita <nix15@bk.ru>
2023-06-01 20:17:49 +0300

    update_task2

---
+++ TASKS/task1/Program.cs
@@ -0,0 +1,3 @@
+﻿Console.Write("Input The Number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(number*number);
---
+++ TASKS/task2/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Input Two Numbers To Compare: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int squad = Convert.ToInt32(Console.ReadLine());
+if (squad == number * number || number == squad * squad)
+{
+    Console.WriteLine("Correct!");
+    if (squad >= number)
+    {
+        Console.Write(squad);
+        Console.Write(" is square of ");
+        Console.WriteLine(number);
+    }
+    else
+    {
+        Console.Write(number);
+        Console.Write(" is square of ");
+        Console.WriteLine(squad);
+    }
+}
+else
+{
+    Console.WriteLine("Error. Try again!");
+}
