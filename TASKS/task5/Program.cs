22db9c917abff2f201896728058db648e69fcd0c
Nikita Sechenov <nikitasechenov@MacBook-Pro-Nikita.local>
2023-06-02 12:17:34 +0300

    5_update

--- TASKS/task5/Program.cs
+++ TASKS/task5/Program.cs
@@ -5,4 +5,4 @@ while (num < -999 || (num > -100 && num < 100) || num > 999)
     Console.Write("Input Your Number Again: ");
     num = Convert.ToInt32(Console.ReadLine());
 }
-Console.WriteLine(num);
+Console.WriteLine(Math.Abs(num%10));
