6eba0f4995d8825ddc51935feff8b174eb89b2af
Nikita Sechenov <nikitasechenov@MacBook-Pro-Nikita.local>
2023-06-02 12:27:43 +0300

    final5

--- TASKS/task5/Program.cs
+++ TASKS/task5/Program.cs
@@ -1,6 +1,6 @@
 ﻿Console.Write("Input Your Number: ");
 int num = Convert.ToInt32(Console.ReadLine());
-while (num < -999 || (num > -100 && num < 100) || num > 999)
+while (Math.Abs(num) < 100 || Math.Abs(num) > 999)
 {
     Console.Write("Input Your Number Again: ");
     num = Convert.ToInt32(Console.ReadLine());
